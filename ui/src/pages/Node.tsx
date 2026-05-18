cb5f92297c657cbc8b810325c5556cb2795a87a4
dmytrolitovskyi <Dmytro.Litovskyi@sogema.com>
2026-05-18 16:24:26 +0200

    Simplify city marker shape

--- ui/src/pages/Node.tsx
+++ ui/src/pages/Node.tsx
@@ -10,15 +10,6 @@ type BuildingProps<T = Color | null> = {
 
 function Building({ building, color }: BuildingProps) {
   const type = building === "CITY" ? "city" : "settlement";
-  if (building === "CITY") {
-    return (
-      <div className={cn(color, type)}>
-        <span className="city-block city-block--base" />
-        <span className="city-block city-block--top" />
-      </div>
-    );
-  }
-
   return <div className={cn(color, type)} />;
 }
 
